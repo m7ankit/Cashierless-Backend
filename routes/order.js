14d38d32858a03ab8404ebcf22747efbda82a4d6
Pranav Jandu <pranav.jandu@gmail.com>
2021-06-17 14:18:48 +0530

    removing update stock as of now

--- routes/order.js
+++ routes/order.js
@@ -1,24 +1,60 @@
 var express = require("express");
 var router = express.Router();
-const { isSignedin, isAuthenticated, isCustomer, isGuard, isManager } = require("../middlewares/auth");
+const {
+  isSignedin,
+  isAuthenticated,
+  isCustomer,
+  isGuard,
+  isManager,
+} = require("../middlewares/auth");
 const { updateStock } = require("../middlewares/product");
 const { getUserById, pushOrderInPurchaseList } = require("../middlewares/user");
-const { createOrder, getAllOrders, getOrderStatus, updateStatus } = require("../controllers/order");
+const {
+  createOrder,
+  getAllOrders,
+  getOrderStatus,
+  updateStatus,
+} = require("../controllers/order");
 const { getOrderById } = require("../middlewares/order");
 
 router.param("userId", getUserById);
 router.param("orderId", getOrderById);
 
 // Get all orders
-router.get("/order/all/:userId", isSignedin, isAuthenticated, isManager, getAllOrders);
+router.get(
+  "/order/all/:userId",
+  isSignedin,
+  isAuthenticated,
+  isManager,
+  getAllOrders
+);
 
 // Get all status in enums
-router.get("/order/status/:userId", isSignedin, isAuthenticated, isManager, getOrderStatus);
+router.get(
+  "/order/status/:userId",
+  isSignedin,
+  isAuthenticated,
+  isManager,
+  getOrderStatus
+);
 
 // Admin can change status of orders
-router.put("/order/status/:userId", isSignedin, isAuthenticated, isManager, updateStatus);
+router.put(
+  "/order/status/:userId",
+  isSignedin,
+  isAuthenticated,
+  isManager,
+  updateStatus
+);
 
 //create a new order createOrder
-router.post("/order/create/:userId", isSignedin, isAuthenticated, isCustomer, pushOrderInPurchaseList, updateStock, createOrder);
+router.post(
+  "/order/create/:userId",
+  isSignedin,
+  isAuthenticated,
+  isCustomer,
+  pushOrderInPurchaseList,
+  createOrder
+);
 
 module.exports = router;
