eb75542add0e8ac1929d28d070a69c8829912748
Pranav Jandu <pranav.jandu@gmail.com>
2021-06-25 23:21:36 +0530

    adding socket apis for video call

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
+
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 var { isUnauthorized } = require("./middlewares/auth");
@@ -17,7 +26,7 @@ const stripeRoutes = require("./routes/stripe");
 
 //DB Connection
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect(process.env.DATABASE_TEST, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -29,6 +38,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.locals.ONLINE_USERS = {};
 
 //Middlewares
 app.use(express.json());
@@ -49,7 +60,52 @@ app.use(isUnauthorized);
 //PORT
 const port = process.env.PORT || 8000;
 
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.emit("me", socket.id);
+
+  socket.on("login", (data) => {
+    console.log(data.email);
+    app.locals.ONLINE_USERS[data.email] = socket.id;
+  });
+
+  socket.on("disconnect", () => {
+    for (const key in app.locals.ONLINE_USERS) {
+      if (app.locals.ONLINE_USERS[key] == socket.id) {
+        delete app.locals.ONLINE_USERS[key];
+        break;
+      }
+    }
+    console.log("A user disconnected");
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    console.log(`From in callUser : ${from}`);
+    for (const key in app.locals.ONLINE_USERS) {
+      if (key == userToCall) {
+        io.to(app.locals.ONLINE_USERS[key]).emit("callUser", {
+          signal: signalData,
+          from,
+          name,
+        });
+        break;
+      }
+    }
+  });
+
+  socket.on("answerCall", (data) => {
+    console.log(`answerCall ${data.to}`);
+    for (const key in app.locals.ONLINE_USERS) {
+      if (key == data.to) {
+        io.to(app.locals.ONLINE_USERS[key]).emit("callAccepted", data.signal);
+        break;
+      }
+    }
+  });
+});
+
 //Starting a server
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`app is running at ${port}`);
 });
